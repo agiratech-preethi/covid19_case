d65ce523c7ba40cbe7b71013a1ee7c583200f838
agiratech-preethi <preethi@agiratech.com>
2020-07-20 21:57:28 +0530

    added env port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const http = require("http");
 const app = require("./tamilnadu-case-node/app");
 const server = http.createServer(app);
-server.listen(4000);
+server.listen(process.env.PORT || 4000);
