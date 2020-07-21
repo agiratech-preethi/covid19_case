5cd03d60302766095e82fa794191810ecdc3d665
agiratech-preethi <preethi@agiratech.com>
2020-07-21 18:41:40 +0530

    removed port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const http = require("http");
 const app = require("./tamilnadu-case-node/app");
 const server = http.createServer(app);
-server.listen(process.env.PORT || 4000);
+server.listen(4000);
